21bc15e3610b93586fe2f2312f1ed94978b5274a
andrescallejasg <andres.callejasg@gmail.com>
2020-04-28 20:53:05 +0000

    duplicated 89 fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=", ")
-print("89")
+        if i == 8 and j == 9:
+            print("89")
+        else:
+            print("{:d}{:d}".format(i, j), end=", ")
+
