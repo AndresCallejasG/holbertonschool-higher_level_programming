392b721c711c9910649c10786d86225c500fb57e
andrescallejasg <andres.callejasg@gmail.com>
2020-04-30 23:51:47 +0000

    my calc fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,13 +14,13 @@ if __name__ == "__main__":
         b = int(av[3])
 
     if av[2] == "*":
-        print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif av[2] == "/":
-        print("{:d} - {:d} = {:d}".format(a, b, div(a, b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     elif av[2] == "+":
-        print("{:d} * {:d} = {:d}".format(a, b, add(a, b)))
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif av[2] == "-":
-        print("{:d} / {:d} = {:d}".format(a, b, sub(a, b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
