baa691ca121c1d54638d3d021cc674fe4f9ca24d
andrescallejasg <andres.callejasg@gmail.com>
2020-04-30 20:30:56 +0000

    fixed

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
-for i in dir(hidden_4):
-    if i[0] == "_" and i[1] == "_":
-        pass
-    else:
-        print(i)
+if __name__ == "__main__":
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[0] == "_" and i[1] == "_":
+            pass
+        else:
+            print(i)
