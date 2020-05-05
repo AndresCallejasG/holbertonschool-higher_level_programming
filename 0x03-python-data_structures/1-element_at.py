d380513e24fcfb56f45787f215eb51ae5a66c493
andrescallejasg <andres.callejasg@gmail.com>
2020-05-05 19:25:10 +0000

    conditional fixed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < len(my_list) and idx >= 0:
-        return my_list[idx]
-    else:
+    if idx >= len(my_list) or idx < 0:
         return none
+    else:
+        return my_list[idx]
