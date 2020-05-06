f87c21d88d8fc542828905f7dd2fdd87be0573f3
andrescallejasg <andres.callejasg@gmail.com>
2020-05-06 01:33:54 +0000

    prints matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        count = 0
+        for i in row:
+            if count == 0:
+                print("{:d}".format(i), end="")
+            else:
+                print(" {:d}".format(i), end="")
+            count += 1
+        print()
