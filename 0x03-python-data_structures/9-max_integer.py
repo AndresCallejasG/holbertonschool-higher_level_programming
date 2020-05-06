dc0af74b5185e5bbc927de555dbebd7811b25637
andrescallejasg <andres.callejasg@gmail.com>
2020-05-06 02:06:11 +0000

    finds max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    else:
+        max = 0
+        for i in my_list:
+            if i > max:
+                max = i
+        return max
