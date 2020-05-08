a739d509fa3962cbea668a24fb6a91499ffd8fc0
andrescallejasg <andres.callejasg@gmail.com>
2020-05-08 02:01:58 +0000

    best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_key = ""
+    max_value = 0
+    if a_dictionary is None:
+        return None
+    for key in a_dictionary:
+        if a_dictionary[key] > max_value:
+            max_value = a_dictionary[key]
+            max_key = key
+    return max_key
