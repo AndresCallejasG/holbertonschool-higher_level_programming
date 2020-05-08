7aac667389dbcf533ff7b26a43888f8502caff0f
andrescallejasg <andres.callejasg@gmail.com>
2020-05-08 02:50:46 +0000

    If dic exists clause

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,9 @@
 def best_score(a_dictionary):
     max_key = ""
     max_value = 0
-    if a_dictionary is None:
-        return None
-    for key in a_dictionary:
-        if a_dictionary[key] > max_value:
-            max_value = a_dictionary[key]
-            max_key = key
-    return max_key
+    if a_dictionary:
+        for key in a_dictionary:
+            if a_dictionary[key] > max_value:
+                max_value = a_dictionary[key]
+                max_key = key
+        return max_key
