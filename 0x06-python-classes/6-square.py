9cb633251939895313994756c18d2ca3a012ee45
andrescallejasg <andres.callejasg@gmail.com>
2020-05-20 02:44:20 +0000

    square with coordinates

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,9 +36,8 @@ class Square:
         if self.__size > 0:
             for i in range(self.__position[1]):
                 print()
-            for i in range(self.__size):
-                print(" " * self.__position[0], end="")
-                print("#" * self.__size)
+            for j in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
         else:
             print()
 
