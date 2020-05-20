203ec48b84f11b8cf2bc41659fde62d1cfee6f3b
andrescallejasg <andres.callejasg@gmail.com>
2020-05-20 02:15:07 +0000

    my_print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" my first class """
+
+
+class Square:
+    """square with size"""
+
+    def __init__(self, size=0):
+        """init function with validation --> positive int"""
+        self.__size = size
+
+    def area(self):
+        """returns the current square area"""
+        my_area = self.__size ** 2
+        return my_area
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size.setter"""
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def my_print(self):
+        """function to print the square using # """
+        if self.__size > 0:
+            for i in range(self.__size):
+                print("#" * self.__size)
+        else:
+            print()
