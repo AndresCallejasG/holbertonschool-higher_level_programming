845e514143be5370cebdb687ac189bd2d7fb66a5
andrescallejasg <andres.callejasg@gmail.com>
2020-06-02 21:33:01 +0000

    area, not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Inheritance project
+
+"""
+
+
+class BaseGeometry():
+    """ Improved Geometry
+    """
+    def area(self):
+        """returns area - not implemented yet
+        """
+        raise Exception("area() is not implemented")
