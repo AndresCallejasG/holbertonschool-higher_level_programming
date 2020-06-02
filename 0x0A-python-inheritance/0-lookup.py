489701581fb3eaea1f3aee4ee7b0ddaf2666ba15
andrescallejasg <andres.callejasg@gmail.com>
2020-06-02 20:33:20 +0000

    object's lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Inheritance project
+
+"""
+
+
+def lookup(obj):
+    """[summary]
+
+    Arguments:
+        obj {object} -- given object to lookup
+    """
+    return(dir(obj))
