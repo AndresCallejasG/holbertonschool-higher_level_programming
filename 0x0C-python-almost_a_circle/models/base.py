56be51728e1c56f877138cb6e4b7f33ab33b4b78
andrescallejasg <andres.callejasg@gmail.com>
2020-06-08 15:49:02 +0000

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,7 +3,10 @@
 
 """
 
-    
+
+import json
+
+
 class Base():
     """ Base class - The goal of it is to manage id attribute in all
         future classes and to avoid duplicating the same code
@@ -22,3 +25,20 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries (dict)
+
+        Returns:
+            If list_dictionaries is None or empty, return the string: "[]"
+            Otherwise, return the JSON string representation of
+            list_dictionaries
+
+        """
+        if list_dictionaries and len(list_dictionaries) > 0:
+            return json.dumps(list_dictionaries)
+        return "[]"
