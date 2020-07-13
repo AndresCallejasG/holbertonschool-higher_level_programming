c1944805d1ce448f2f898c9051e4cffbe1b90c8c
andrescallejasg <andres.callejasg@gmail.com>
2020-07-13 23:18:16 +0000

    No idea what happened, right version

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,2 @@
--- lists all records of the table second_table of the database
--- Don’t list rows without a name value, Records should be listed by descending score
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+-- lists the number of records with the same score in the table second_table
+SELECT score, COUNT(*) number FROM second_table GROUP BY score ORDER BY COUNT(*) DESC;
