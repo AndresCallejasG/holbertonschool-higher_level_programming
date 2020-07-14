90c5091007f914810028bbecc48e9284bdbd8244
andrescallejasg <andres.callejasg@gmail.com>
2020-07-14 23:58:43 +0000

    fixed comments

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- SQL - More queries
--- lists all shows contained in the database hbtn_0d_tvshows
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id 
     FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     WHERE tv_show_genres.genre_id IS NULL
