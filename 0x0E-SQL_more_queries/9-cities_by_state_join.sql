c6d9774bde654120102a4d7eb4491a260d170cf3
andrescallejasg <andres.callejasg@gmail.com>
2020-07-14 22:03:15 +0000

    Join fixed

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- SQL - More queries
 -- lists all the cities of California that can be found in the database using join
 SELECT cities.id, cities.name, states.name
-    FROM cities JOIN states ON cities.id = states.id
+    FROM cities JOIN states ON states.id = cities.state_id
     ORDER BY cities.id; 
