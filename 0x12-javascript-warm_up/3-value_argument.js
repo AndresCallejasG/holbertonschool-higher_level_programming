aa0ca76481601c02d42f48fec32bdbeecbebaee4
andrescallejasg <andres.callejasg@gmail.com>
2020-09-15 19:06:22 +0000

    print arg- no length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,8 @@
  * prints the first argument passed to it
  * If no arguments are passed to the script, print “No argument”
  */
-if (process.argv.length <= 2) {
-  console.log('No argument');
-} else {
+if (process.argv[2]) {
   console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
