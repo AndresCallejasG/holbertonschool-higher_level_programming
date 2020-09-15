49ff528cf0f5c5189000ccd79abd91f882b58ad0
andrescallejasg <andres.callejasg@gmail.com>
2020-09-15 16:17:29 +0000

    semistandard checked

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@
  * depending of the number of arguments passed
  */
 if (process.argv.length <= 2) {
-    console.log('No argument')
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-    console.log('Argument found')
+  console.log('Argument found');
 } else {
-    console.log('Arguments found')
+  console.log('Arguments found');
 }
