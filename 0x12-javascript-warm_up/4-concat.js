cbd65241108321bc7f47417a7402a0900416d098
andrescallejasg <andres.callejasg@gmail.com>
2020-09-15 19:10:23 +0000

    concat

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/**
+ * prints two arguments passed to it
+ * arg1 + " is " + arg2
+ * If no arguments are passed to the script, print “No argument”
+ */
+console.log(process.argv[2] + ' is ' + process.argv[3]);
