46eeab707de9bb3c6c74608317c8515299dd60d3
andrescallejasg <andres.callejasg@gmail.com>
2020-09-15 19:10:52 +0000

    concat comments fixed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,6 +2,5 @@
 /**
  * prints two arguments passed to it
  * arg1 + " is " + arg2
- * If no arguments are passed to the script, print “No argument”
  */
 console.log(process.argv[2] + ' is ' + process.argv[3]);
