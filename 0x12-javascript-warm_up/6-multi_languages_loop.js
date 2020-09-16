1dbce8804b2d8e93e7932d8ee4ad948ef3701dda
andrescallejasg <andres.callejasg@gmail.com>
2020-09-16 03:10:00 +0000

    array and foreach

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/**
+ * prints 3 lines: "C is fun” ,“Python is cool” and
+ * “Javascript is amazing” using an array and a Loop
+ */
+const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+lang.forEach(elem => console.log(elem));
