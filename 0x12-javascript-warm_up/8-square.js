6b0f6b624dd8b6ec92207901382cc2c9782624fb
andrescallejasg <andres.callejasg@gmail.com>
2020-09-16 04:01:58 +0000

    print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/**
+ * script that prints a square using the character X
+ * If the first arg can’t be converted to an integer print “Missing size”
+ *
+ */
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const size = parseInt(process.argv[2]);
+  for (let i = 0; i < size; i++) {
+    let line = '';
+    for (let j = 0; j < size; j++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+}
