bdb34ecc0d9081ad527f05de3ce0b19b6908f0b8
andrescallejasg <andres.callejasg@gmail.com>
2020-09-16 03:03:07 +0000

    parse to int

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * prints My number: <first argument converted in integer>
+ * if the first argument can be converted to an integer
+ */
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
