5cd04fa7ead344a8a1531247f76c96158ad68491
andrescallejasg <andres.callejasg@gmail.com>
2020-09-22 01:19:44 +0000

    1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * class Rectangle, takes 2 arguments w and h
+ * using the class notation for defining
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
