13585d7d0e8be688d908c1465ebc35648bd3cdb0
andrescallejasg <andres.callejasg@gmail.com>
2020-09-25 15:51:56 +0000

    Functions rotate and double

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/**
+ * class Rectangle, takes 2 arguments w and h
+ * If w or h is equal to 0 or not a positive integer,
+ * create an empty object
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
