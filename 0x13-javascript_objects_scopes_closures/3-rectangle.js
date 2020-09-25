81ce6558d1ed63c5b6aa25c519a78d8774054c7a
andrescallejasg <andres.callejasg@gmail.com>
2020-09-25 15:46:56 +0000

    rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/**
+ * class Rectangle, takes 2 arguments w and h
+ * If w or h is equal to 0 or not a positive integer,
+ * create an empty object
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
