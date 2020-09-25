0cf7edae764a0780773cd4ec3a6e6b3d8fa3fc7d
andrescallejasg <andres.callejasg@gmail.com>
2020-09-25 16:02:14 +0000

    square#1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+module.exports = Square;
